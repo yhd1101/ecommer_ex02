8a3bd1d0a4ea9280877e471809f654a90f8c6b0a
yhd1101 <asd159369@naver.com>
2023-03-18 02:32:45 +0900

    ecommer_ex02_database_review

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ import productRoutes from "./route/product.js"
 import product from "./route/product.js";
 import orderRouters from "./route/oreder.js"
 import morgan from "morgan"
+import bodyParser from "body-parser";
+import dotEnv from "dotenv"
+import mongoose from "mongoose";
 
 
 
 const app = express()
+dotEnv.config()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
 
 app.use(morgan("common"))
+app.use("/product", productRoutes)
+app.use("/order", orderRouters)
 
+//데이터베이스 연결정보
+
+const dbAddress = process.env.MONGODB_ADDRESS
+mongoose
+    .connect(dbAddress)
+    .then(_ => console.log("database connected"))
+    .catch(err => console.log(err.message))
 
 
 
@@ -24,9 +40,6 @@ app.use("/hello", (req, res) => {
 })
 
 
-app.use("/product", productRoutes)
-app.use("/order", orderRouters)
 
-
-const port = 7000
+const port = process.env.PORT || 8500
 app.listen(port, console.log("Server started"))
--- route/product.js
+++ route/product.js
@@ -1,17 +1,90 @@
 import express from "express";
+import productModel from "../models/product.js";
+import product from "../models/product.js";
+
 
 const router = express.Router()
 
+
+
+//product 전체를 불러오는 api
 router.get("/", (req, res) =>{
-    res.json({
-        msg : "product get all"
-    })
+    productModel
+        .find()
+        .then(products =>{
+            res.json({
+                msg : "get all products",
+                count : products.length,
+                products : products
+            })
+        })
+        .catch(err => {
+            res.json({
+                meg : err.message
+            })
+        })
+})
+//특정 product를 불러오는 api
+router.get("/:productid", (req , res) =>{
+    productModel
+        .findById(req.params.productid)
+        .then(product => {
+            if(!product){
+                return res.json({
+                    msg : "No data"
+                })
+            }
+            res.json({
+                msg : "Successful get product",
+                product : product
+            })
+        })
+        .catch(err => {
+            res.json({
+                mes : err.message
+            })
+        })
+
 })
 
-router.post("/create", (req, res) =>{
-    res.json({
-        msg : "created a product"
+//product 등록하는 api
+router.post("/create", (req, res) => {
+    // const newProduct = {
+    //     name : req.body.productName,
+    //     price : req.body.productPrice,
+    //     desc : req.body.productDesc
+    // }
+
+    const newProduct = new productModel({
+        name : req.body.productName,
+        price : req.body.productPrice,
+        desc : req.body.productDesc,
+        category : req.body.productCategory
     })
+    newProduct
+        .save()
+        .then(result => {
+            res.json({
+                msg : "Sucessful create a user",
+                user : {
+                    name : result.name,
+                    price : result.price,
+                    desc : result.desc,
+                    id : result._id
+                }
+            })
+        })
+        .catch(err =>{
+            res.json({
+                msg : err.message
+            })
+    })
+
+
+    // res.json({
+    //     msg : "created a product",
+    //     product : newProduct
+    // })
 })
 
 router.put("/update", (req, res) =>{
@@ -19,11 +92,36 @@ router.put("/update", (req, res) =>{
         msg : "update a product"
     })
 })
+//product 전체를 삭제하는 api
+router.delete("/", (req, res) =>{
+    productModel
+        .deleteMany()
+        .then(_=> {
+            res.json({
+                msg : "deleted all product"
+            })
 
-router.delete("/delete", (req, res) => {
-    res.json({
-        msg : "deleted a product"
-    })
+        })
+        .catch(err => {
+            res.json({
+                msg : err.message
+            })
+        })
+})
+
+router.delete("/:prdoductid", (req , res) =>{
+    productModel
+        .findByIdAndDelete(req.params.productid)
+        .then(_=> {
+            res.json({
+                msg :"deleted product"
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg : err.message
+            })
+        })
 })
 
 
